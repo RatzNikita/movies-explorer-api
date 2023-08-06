9a2146e83bb8eb0b429c4211c0df1ac2cc8611d0
Ratz Nikita <ratsnikita163@gmail.com>
2023-08-06 11:34:34 +0400

    add controllers

---
+++ controllers/movies.js
@@ -0,0 +1,32 @@
+const Movie = require('../models/movie');
+const { handleException } = require('../exceptions/exceptions');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({})
+    .populate('owner')
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+module.exports.postMovie = (req, res, next) => {
+  const owner = req.user._id;
+  Movie.create({ ...req.body, owner })
+    .then((movie) => {
+      movie.populate('owner').then((populatedMovie) => res.status(201).send(populatedMovie));
+    })
+    .catch(next);
+};
+
+module.exports.removeMovie = (req, res, next) => {
+  Movie.findById({ _id: req.params._id })
+    .then((movie) => {
+      if (!movie) {
+        handleException({ name: 'NotFound' }, req, res);
+      } else if (movie.owner.equals(req.user._id)) {
+        movie.deleteOne().then(() => res.send({ message: 'Фильм удалён' }));
+      } else {
+        handleException({ name: 'NotPermissions' }, req, res);
+      }
+    })
+    .catch(next);
+};
