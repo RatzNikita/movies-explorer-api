3aab42cdfcd5c809da9afff8bf9ef34d8b218bf6
Ratz Nikita <ratsnikita163@gmail.com>
2023-08-06 11:34:25 +0400

    add routes

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const router = require('express').Router();
+const {
+  updateProfile, getUserInfo,
+} = require('../controllers/users');
+const { updateProfileValidation } = require('../validation/celebrateSchemas');
+
+router.get('/me', getUserInfo);
+router.patch('/me', updateProfileValidation, updateProfile);
+
+module.exports = router;
